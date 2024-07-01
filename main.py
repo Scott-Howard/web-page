d73cfb6cf90721eb427b08b49bfd58d4c1aab4f9
Scott Howard <scottmelvillehoward@gmail.com>
2024-07-01 21:12:39 +1000

    update some errors etc

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.png", width=600)
     
-    with col2:
-        st.title("Scott Howard")
-        content = """
-        Welcome to my website. I am a Data Scientist and Statistician. 
-        I have experience in Python, R, SQL, and machine learning. 
-        I have a passion for data and I am always looking for new projects to work on. 
-        Feel free to reach out to me if you have any questions or would like to work together.
-        """
-        st.info(content)
+with col2:
+    st.title("Scott Howard")
+    content = """
+    Welcome to my website. I am a Data Scientist and Statistician. 
+    I have experience in Python, R, SQL, and machine learning. 
+    I have a passion for data and I am always looking for new projects to work on. 
+    Feel free to reach out to me if you have any questions or would like to work together.
+    """
+    st.info(content)
 
 tagline = """
 Below you can find some of the apps I have built in Python. Feel free to explore them and let me know what you think.
 """
-st.info(tagline)
+st.write(tagline)
     
