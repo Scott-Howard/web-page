1db2bb6d65850cbc217a202b9ade23ff81f249b0
Scott Howard <scottmelvillehoward@gmail.com>
2024-07-14 22:08:44 +1000

    updated to a pic of myself

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=600)
+    st.image("images/sh_google.jpg", width=600)
     
 with col2:
     st.title("Scott Howard")
