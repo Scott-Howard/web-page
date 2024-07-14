21b943c9940bdd2f05ac6f125df6e3ed56f4b2e4
Scott Howard <scottmelvillehoward@gmail.com>
2024-07-14 22:31:10 +1000

    added columns with projects to webpage

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # code a contact me page for the website
 #add a navigation bar to the website
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -14,6 +15,7 @@ with col2:
     st.title("Scott Howard")
     content = """
     Welcome to my website. I am a Data Scientist and Statistician. 
+    I graduated in 2022 from the University of Newcastle with a Honours degree in Statistics.
     I have experience in Python, R, SQL, and machine learning. 
     I have a passion for data and I am always looking for new projects to work on. 
     I have worked on projects in a variety of industries including Education, Agribuisness, and Supply chains.
@@ -26,3 +28,14 @@ Below you can find some of the apps I have built in Python. Feel free to explore
 """
 st.write(tagline)
     
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
